1b8dc7a3d0de06a1e9e28f358eb273fdd763d49f
KSH <k@k>
2020-05-20 22:07:53 -0500

    em 2

--- em.cpp
+++ em.cpp
@@ -4,21 +4,18 @@
 
 #include <kora/Model.h>
 
-#include <emscripten/em_macros.h>
+#include <emscripten/bind.h>
+
 #include <cstring>
 
 using namespace kora;
 
-extern "C" {
-
-EMSCRIPTEN_KEEPALIVE char *analyze_fba(const char *json) {
+str analyze_fba(str json) {
     Model m;
-    m.load_from_json(json);
-
-    str sret = m.fba();
-    char *ret = (char *) malloc(sz(sret) + 1);
-    memcpy(ret, sret.c_str(), sz(sret) + 1);
-    return ret;
+    m.load_from_json(move(json));
+    return m.fba();
 }
 
+EMSCRIPTEN_BINDINGS(sh_kora) {
+    emscripten::function("analyze_fba", &analyze_fba);
 }
